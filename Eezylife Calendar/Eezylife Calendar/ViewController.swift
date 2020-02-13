cbb1519fdc0423df195be2715ee5fc7af2040dac
saurabh-amura <saurabh.bajaj@sell.do>
2020-02-13 22:03:57 +0530

    initial commit

---
+++ Eezylife Calendar/Eezylife Calendar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Eezylife Calendar
+//
+//  Created by Saurabh Bajaj on 13/02/20.
+//  Copyright © 2020 Saurabh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
